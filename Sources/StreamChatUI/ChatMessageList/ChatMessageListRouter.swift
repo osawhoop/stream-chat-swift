25ce7cbb53b5b6a1e17058135c3bb42d53b0fcb8
marek.fort <marek.fort@ackee.cz>
2021-05-03 09:08:55 +0200

    Animate blur and reactions

--- Sources/StreamChatUI/ChatMessageList/ChatMessageListRouter.swift
+++ Sources/StreamChatUI/ChatMessageList/ChatMessageListRouter.swift
@@ -146,16 +146,16 @@ open class MessageActionsTransitionController<ExtraData: ExtraDataTypes>: NSObje
         
         transitionContext.containerView.addSubview(toVC.view)
         
-        let blurSnapshot = toVC.view.snapshotView(afterScreenUpdates: true)!
-        blurSnapshot.frame = toVC.view.frame
-        transitionContext.containerView.addSubview(blurSnapshot)
-        
         let fromVCSnapshot = fromVC.view.snapshotView(afterScreenUpdates: true)
         if let fromVCSnapshot = fromVCSnapshot {
             transitionContext.containerView.addSubview(fromVCSnapshot)
             fromVCSnapshot.frame = fromVC.view.frame
             fromVC.view.isHidden = true
         }
+
+        let blurView = UIVisualEffectView()
+        blurView.frame = toVC.view.frame
+        transitionContext.containerView.addSubview(blurView)
         
 //        let toVCSnapshot = toVC.view.snapshotView(afterScreenUpdates: true)
 //        if let toVCSnapshot = toVCSnapshot {
@@ -164,6 +164,17 @@ open class MessageActionsTransitionController<ExtraData: ExtraDataTypes>: NSObje
 //            toVCSnapshot.alpha = 0
 //            toVC.view.isHidden = true
 //        }
+        
+        let reactionsSnapshot: UIView?
+        if let reactionsController = toVC.reactionsController {
+            reactionsSnapshot = reactionsController.view.snapshotView(afterScreenUpdates: true)
+            reactionsSnapshot?.frame = reactionsController.view.superview!.convert(reactionsController.view.frame, to: nil)
+            reactionsSnapshot?.transform = CGAffineTransform(scaleX: 0, y: 0)
+            reactionsSnapshot.map(transitionContext.containerView.addSubview)
+        } else {
+            reactionsSnapshot = nil
+        }
+        
         let actionsSnapshot = toVC.actionsController.view.snapshotView(afterScreenUpdates: true)!
         let actionsFrame = toVC.actionsController.view.superview!.convert(toVC.actionsController.view.frame, to: nil)
         actionsSnapshot.frame = actionsFrame
@@ -183,15 +194,20 @@ open class MessageActionsTransitionController<ExtraData: ExtraDataTypes>: NSObje
         UIView.animate(
             withDuration: duration,
             delay: 0,
-            usingSpringWithDamping: 0.8,
+            usingSpringWithDamping: 0.9,
             initialSpringVelocity: 4,
             animations: { [self] in
                 actionsSnapshot.transform = .identity
+                reactionsSnapshot?.transform = .identity
                 messageContentView.frame.origin = toVC.messageContentContainerView.superview!.convert(
                     toVC.messageContentContainerView.frame,
                     to: nil
-                ).origin
+                )
+                .origin
 //                fromVCSnapshot?.alpha = 0
+                if let effect = (toVC.blurView as? UIVisualEffectView)?.effect {
+                    blurView.effect = effect
+                }
             },
             completion: { [self] _ in
                 toVC.view.isHidden = false
@@ -203,7 +219,9 @@ open class MessageActionsTransitionController<ExtraData: ExtraDataTypes>: NSObje
 //                toVC.messageContentView.isHidden = false
 //                transitionMessageContentView.removeFromSuperview()
                 fromVCSnapshot?.removeFromSuperview()
-                blurSnapshot.removeFromSuperview()
+                blurView.removeFromSuperview()
+                reactionsSnapshot?.removeFromSuperview()
+                actionsSnapshot.removeFromSuperview()
                 
                 transitionContext.completeTransition(!transitionContext.transitionWasCancelled)
             }
